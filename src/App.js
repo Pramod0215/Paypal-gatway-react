6e2fe4ad03ee22d5d7e8a38408fcc8cf4f35e8ac
Pramod0215 <pramodray0215@gmail.com>
2021-02-22 17:42:49 +0530

    paypal gateway

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PaypalButtons from './Paypal/Paypal'
+
+class App extends React.Component {
+  state = {
+    showPaypal: false
+  };
+
+  showPaypalButtons = () => {
+    this.setState({ showPaypal: true });
+  };
+
+  render() {
+    const { showPaypal } = this.state;
+    if (showPaypal) {
+      return <PaypalButtons />;
+    } else {
+      return (
+        <div className="main">
+          <h2> Buy this Mercedes at a giveaway price (Super Cheap) </h2>
+          {/* <img alt="Mercedes G-Wagon" src={Car} /> */}
+          <h3>
+            <b>$200</b>
+          </h3>
+          <button onClick={this.showPaypalButtons}> Pay </button>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
